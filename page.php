26dacb8ebe2668b4d4504bdcbd94659ece42452c
varffs <patrickbest@Patricks-MacBook-Pro.local>
2019-01-03 19:03:34 +0000

    basic scaffold page.php

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+?>
+
+<main id="main-content">
+  <section id="page">
+    <div class="container">
+
+<?php
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+?>
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+      <section id="single-post-text" class="margin-top-basic margin-bottom-large">
+        <div class="grid-row">
+          <div class="grid-item item-s-9 offset-s-3 margin-bottom-basic">
+            <h1><?php the_title(); ?></h1>
+          </div>
+        </div>
+      </section>
+
+      <section class="grid-row">
+        <div class="grid-item item-s-12">
+          <?php the_content(); ?>
+        </div>
+      </section>
+
+    </article>
+
+<?php
+  }
+} ?>
+    </div>
+  </section>
+
+</main>
+
+<?php
+get_footer();
+?>
