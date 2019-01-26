c339696dc82577b7ffd487f02bb0de65a28fe235
varffs <patrickbest@Patricks-MacBook-Pro.local>
2019-01-26 17:31:24 +0000

    Add multiple showreel support

--- page-showreel.php
+++ page-showreel.php
@@ -11,32 +11,44 @@ if (have_posts()) {
   while (have_posts()) {
     the_post();
 
-    $vimeo = get_post_meta($post->ID, '_igv_vimeo', true);
-    $credits = get_post_meta($post->ID, '_igv_credits', true);
+    $showreels = get_post_meta($post->ID, '_igv_showreels', true);
 ?>
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-      <section id="single-post-player" class="grid-row">
-        <div class="grid-item item-s-12">
-          <?php if ($vimeo) { ?>
-            <div data-vimeo-url="<?php echo $vimeo; ?>" id="single-post-vimeo" class="u-video-embed-container"></div>
-          <?php } ?>
-        </div>
-      </section>
-
-      <section id="single-post-text" class="margin-top-basic margin-bottom-large">
-        <div class="grid-row">
-          <div class="grid-item item-s-9 offset-s-3 margin-bottom-basic">
-            <h1><?php the_title(); ?></h1>
-          </div>
-        </div>
-      </section>
-
       <section class="grid-row">
         <div class="grid-item item-s-12">
           <?php the_content(); ?>
         </div>
       </section>
+
+      <?php
+        if ($showreels) {
+          $length = count($showreels);
+          $i = 1;
+          foreach($showreels as $showreel) {
+      ?>
+      <section id="single-post-player" class="grid-row">
+        <div class="grid-item item-s-12">
+          <?php if ($showreel['vimeo']) { ?>
+            <div data-vimeo-url="<?php echo $showreel['vimeo']; ?>" id="single-post-vimeo" class="u-video-embed-container"></div>
+          <?php } ?>
+        </div>
+      </section>
+
+      <section id="single-post-text" class="margin-top-basic <?php if ($i !== $length) {echo 'margin-bottom-large';} ?>">
+        <div class="grid-row">
+          <div class="grid-item item-s-9 offset-s-3 margin-bottom-basic">
+          <?php if ($showreel['title']) { ?>
+            <h1><?php echo $showreel['title']; ?></h1>
+          <?php } ?>
+          </div>
+        </div>
+      </section>
+      <?php
+            $i++;
+          }
+        }
+      ?>
 
     </article>
 
