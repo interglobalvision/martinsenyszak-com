5f77510cb571f23adc0becd13a487ad2290140ab
varffs <patrickbest@Patricks-MacBook-Pro.local>
2019-02-01 12:06:37 +0000

    Detail: Small mobile header
    Make text small on 320 width devices and jmup back for 375

--- header.php
+++ header.php
@@ -33,14 +33,14 @@ get_template_part('partials/seo');
 <header id="header">
   <div class="container margin-top-small">
     <div class="grid-row align-items-baseline">
-      <div class="grid-item item-s-5">
+      <div class="grid-item item-s-4 item-m-5">
         <a href="<?php echo home_url(); ?>">
           <div id="header-logotype">
             <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/martinsensyzak.svg'); ?>
           </div>
         </a>
       </div>
-      <nav id="menu" class="grid-item item-s-6 offset-s-1 item-m-5 offset-m-2">
+      <nav id="menu" class="grid-item item-s-7 offset-s-1 item-m-5 offset-m-2">
         <ul class="grid-row justify-between">
           <li <?php set_menu_active_classes('grid-item no-gutter', 'work'); ?>><a href="<?php echo home_url(); ?>">Work</a></li>
           <li <?php set_menu_active_classes('grid-item no-gutter', 'page', 'showreel'); ?>><a href="<?php echo home_url('showreel'); ?>">Showreel</a></li>
